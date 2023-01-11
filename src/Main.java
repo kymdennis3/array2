0625d61a0a346cf550e2b1d84f7611a22929a794
Kimtai <kymdennis3@gmail.com>
2023-01-11 06:40:32 +0300

    reassigning a variable in the array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String [] cars = {"Camaro", "Tesla", "Ford"};
+        cars[0]= "Toyota";
+        System.out.println(cars[0]);
     }
 }
